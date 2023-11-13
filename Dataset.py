a4b88ecf3eeb800d191c5f8b70c6a3b9c67e2505
Aleksander Samek <olek.samek@gmail.com>
2023-11-13 19:51:36 +0100

    incremental elm

--- Dataset.py
+++ Dataset.py
@@ -10,3 +10,7 @@ class Dataset(torch.utils.data.Dataset):
     
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
+
+# ELM with fc2 bias
+# V = activation(model.fc1.bias.data.reshape(-1, 1) + model.fc1.weight.data @ trainset[:][0].reshape(1, -1))
+# H = torch.concatenate([torch.ones(1, len(trainset)), V], dim=0).T
