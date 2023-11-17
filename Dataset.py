d113d965622ba73de4678e5fbb715a1a876d2d5d
Aleksander Samek <olek.samek@gmail.com>
2023-11-17 12:22:22 +0100

    float64

--- Dataset.py
+++ Dataset.py
@@ -2,14 +2,15 @@ import torch
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, x, y):
-        self.x = torch.FloatTensor(x)
-        self.y = torch.FloatTensor(y)
+        self.x = torch.Tensor(x)
+        self.y = torch.Tensor(y)
     
     def __len__(self):
         return len(self.x)
     
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
+
 
 # ELM with fc2 bias
 # V = activation(model.fc1.bias.data.reshape(-1, 1) + model.fc1.weight.data @ interset[:][0].reshape(1, -1))
